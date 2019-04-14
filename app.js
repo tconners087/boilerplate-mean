3a80f64fc14194bc74dea2aca8a961453d1882cf
Taylor Conners <tconners@uncc.edu>
2019-04-14 13:32:48 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/popper')));
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'MEAN boilerplate' });
+});
+
+app.listen(port, () => {
+  debug(`listening at port ${chalk.green(port)}`);
+});
